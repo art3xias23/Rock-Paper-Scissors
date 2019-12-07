60f040d8f23fbbdf22a79f77c7f2d19afc2f724b
Konstantin Milchev <tino@debian>
2019-12-07 16:45:42 +0000

    added battle field icons, score and app.js file

---
+++ app.js
@@ -0,0 +1,6 @@
+var userScore = 0;
+var computerScore = 0;
+
+const rockChoice = document.getElementById("rock");
+const paperChoice = document.getElementById("paper");
+const scissorsChoice = document.getElementById("scissors");
